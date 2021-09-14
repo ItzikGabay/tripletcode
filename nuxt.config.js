938c82c931d410fb5034eed01108540e8fd7fbf6
Itzik <itzikdevio@gmail.com>
2021-09-14 16:14:34 +0300

    vercel v3

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,7 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
+  target: 'static', // default is 'server'
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
